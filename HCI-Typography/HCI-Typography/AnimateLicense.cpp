26c82d94e3e3753553151699c64fbba9667d480f
Emmett Byrne <C00220950@itcarlow.ie>
2017-11-28 13:01:52 +0000

    starting next anim

--- HCI-Typography/HCI-Typography/AnimateLicense.cpp
+++ HCI-Typography/HCI-Typography/AnimateLicense.cpp
@@ -1,12 +1,105 @@
+// author Emmett Byrne
+// date 28-11-17
 #include "AnimateLicense.h"
 
 
 
-AnimateLicense::AnimateLicense()
+AnimateLicense::AnimateLicense(sf::Vector2f tPos, sf::Vector2f sPos) :
+	m_paused{ false },
+	m_currentKeyFrame{ 0 }
 {
-}
 
+	if (!m_font.loadFromFile("ASSETS\\FONTS\\AYearWithoutRain.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+	m_title.setFont(m_font);
+	m_sub.setFont(m_font);
+
+	m_title.setString("TitleText");
+	m_sub.setString("SubText");
+
+	m_title.setStyle(sf::Text::Bold);
+
+	m_title.setCharacterSize(30);
+	m_sub.setCharacterSize(20);
+
+	m_title.setOrigin(m_title.getLocalBounds().width / 2, m_title.getLocalBounds().height / 2);
+	m_sub.setOrigin(m_sub.getLocalBounds().width / 2, m_sub.getLocalBounds().height / 2);
+
+	m_keyframe[0].m_startPositionTitle = tPos;
+	m_keyframe[0].m_startPositionSub = sPos;
+
+	m_titlePosition = tPos;
+	m_subPosition = sPos;
+}
 
 AnimateLicense::~AnimateLicense()
 {
+}
+
+void AnimateLicense::update(sf::Time t)
+{
+	if (!m_paused)
+	{
+		if (m_keyframe[m_currentKeyFrame].m_timePassed < m_keyframe[m_currentKeyFrame].m_duration)
+		{
+			sf::Vector2f moveTitle(m_keyframe[m_currentKeyFrame].m_targetPositionTitle - m_keyframe[m_currentKeyFrame].m_startPositionTitle);
+			sf::Vector2f moveSub(m_keyframe[m_currentKeyFrame].m_targetPositionSub - m_keyframe[m_currentKeyFrame].m_startPositionSub);
+
+			float scaler = m_keyframe[m_currentKeyFrame].m_duration.asSeconds() / t.asSeconds();
+			moveTitle /= scaler;
+			moveSub /= scaler;
+
+			m_titlePosition += moveTitle;
+			m_subPosition += moveSub;
+
+			m_keyframe[m_currentKeyFrame].m_timePassed += t;
+		}
+		else
+		{
+			if (m_currentKeyFrame < 2)
+			{
+				m_titlePosition = m_keyframe[m_currentKeyFrame].m_targetPositionTitle;
+				m_subPosition = m_keyframe[m_currentKeyFrame].m_targetPositionSub;
+
+				m_keyframe[m_currentKeyFrame + 1].m_startPositionTitle = m_keyframe[m_currentKeyFrame].m_targetPositionTitle;
+				m_keyframe[m_currentKeyFrame + 1].m_startPositionSub = m_keyframe[m_currentKeyFrame].m_targetPositionSub;
+
+				m_currentKeyFrame++;
+			}
+			else
+			{
+				m_paused = true;
+			}
+		}
+	}
+	else
+	{
+		std::cout << "Paused" << std::endl;
+	}
+
+
+	m_title.setPosition(m_titlePosition);
+	m_sub.setPosition(m_subPosition);
+}
+
+void AnimateLicense::render(sf::RenderWindow & window)
+{
+	window.draw(m_title);
+	window.draw(m_sub);
+}
+
+void AnimateLicense::setKeyframe(int key, sf::Time dur, sf::Vector2f tPos, sf::Vector2f sPos, AnimType type)
+{
+	m_keyframe[key].m_duration = dur;
+	m_keyframe[key].m_timePassed = sf::Time::Zero;
+	m_keyframe[key].m_targetPositionTitle = tPos;
+	m_keyframe[key].m_targetPositionSub = sPos;
+	m_keyframe[key].m_type = type;
+}
+
+bool AnimateLicense::isPaused()
+{
+	return m_paused;
 }
--- HCI-Typography/HCI-Typography/AnimateLicense.h
+++ HCI-Typography/HCI-Typography/AnimateLicense.h
@@ -1,8 +1,50 @@
+// author Emmett Byrne
+// date 28-11-17
 #pragma once
+#include <iostream>
+#include <SFML\Graphics.hpp>
+
+enum class AnimType
+{
+	move,
+	somersault
+};
+
+struct KeyFrame
+{
+	sf::Time m_duration; //How long the keyframe lasts
+	sf::Time m_timePassed; //The amount of time that the keyframe has been playing for
+	sf::Vector2f m_startPositionTitle; //The starting position of the title for the keyframe
+	sf::Vector2f m_targetPositionTitle; //The end position of the title for the keyframe
+	sf::Vector2f m_startPositionSub; //The starting position of the subText for the keyframe
+	sf::Vector2f m_targetPositionSub; //The end position of the subText for the keyframe
+	AnimType m_type;
+};
+
 class AnimateLicense
 {
 public:
-	AnimateLicense();
+	AnimateLicense(sf::Vector2f tPos, sf::Vector2f sPos);
 	~AnimateLicense();
+
+	void update(sf::Time t);
+	void render(sf::RenderWindow &window);
+
+	void setKeyframe(int key, sf::Time dur, sf::Vector2f tPos, sf::Vector2f sPos, AnimType type);
+
+	bool isPaused();
+
+private:
+	bool m_paused; //Is the animation paused
+
+	sf::Font m_font; //Text font being used
+	sf::Text m_title; //The title text
+	sf::Text m_sub; //The Subtext
+
+	sf::Vector2f m_titlePosition; //position of title text
+	sf::Vector2f m_subPosition; //position of sub text
+
+	KeyFrame m_keyframe[4]; //array of all the keyframes
+	int m_currentKeyFrame; //the current keyframe that is playing
 };
 
