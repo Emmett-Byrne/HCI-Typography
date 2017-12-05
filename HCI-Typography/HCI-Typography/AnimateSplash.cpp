c33b3915d90f79d1883cfb8a01e1d19e7838ebbd
Emmett <C00220950@itcarlow.ie>
2017-12-05 00:31:17 +0000

    4th anim fin

--- HCI-Typography/HCI-Typography/AnimateSplash.cpp
+++ HCI-Typography/HCI-Typography/AnimateSplash.cpp
@@ -5,7 +5,11 @@
 AnimateSplash::AnimateSplash() :
 	m_spritePosition(400, -300),
 	m_textPosition(900, 300),
-	m_gravity (0, 9.8f * 400)
+	m_textStart(900, 300),
+	m_slideTarget(400, 300),
+	m_exitTarget(400, -300),
+	m_gravity (0, 9.8f * 400),
+	m_currentKeyFrame(0)
 {
 	if (!m_texture.loadFromFile("ASSETS\\IMAGES\\background.jpg"))
 	{
@@ -24,6 +28,9 @@ AnimateSplash::AnimateSplash() :
 	m_text.setStyle(sf::Text::Bold);
 	m_text.setCharacterSize(30);
 	m_text.setOrigin(m_text.getLocalBounds().width / 2, m_text.getLocalBounds().height / 2);
+
+	m_slideDuration = sf::seconds(1.0f);
+	m_exitDuration = sf::seconds(1.0f);
 }
 
 
@@ -33,23 +40,21 @@ AnimateSplash::~AnimateSplash()
 
 void AnimateSplash::update(sf::Time t)
 {
-	m_velocity = m_velocity + m_gravity * t.asSeconds();
-	m_spritePosition += m_velocity * t.asSeconds() + (m_gravity * (t.asSeconds() * t.asSeconds()) * 0.5f);
+	if (m_currentKeyFrame == 0)
+	{
+		fall(t);
+	}
+	if (m_currentKeyFrame == 1)
+	{
+		slide(t);
+	}
+	if (m_currentKeyFrame == 2)
+	{
+		exit(t);
+	}
 
 	m_text.setPosition(m_textPosition);
 	m_background.setPosition(m_spritePosition);
-	if (m_spritePosition.y > 300)
-	{
-		m_velocity *= -1.0f;
-		m_velocity *= 0.5f;
-		if (m_velocity.y < 2.0f && m_velocity.y > -2.0f)
-		{
-			m_velocity.y = 0;
-		}
-		m_spritePosition.y = 300;
-
-		std::cout << m_velocity.y << std::endl;
-	}
 }
 
 void AnimateSplash::render(sf::RenderWindow & window)
@@ -66,4 +71,68 @@ bool AnimateSplash::isPaused()
 bool AnimateSplash::isFinished()
 {
 	return m_finished;
+}
+
+void AnimateSplash::play()
+{
+	m_paused = true;
+}
+
+void AnimateSplash::fall(sf::Time t)
+{
+	m_velocity = m_velocity + m_gravity * t.asSeconds();
+	m_spritePosition += m_velocity * t.asSeconds() + (m_gravity * (t.asSeconds() * t.asSeconds()) * 0.5f);
+
+	if (m_spritePosition.y > 300)
+	{
+		m_velocity *= -1.0f;
+		m_velocity *= 0.5f;
+		if (m_velocity.y < 25.0f && m_velocity.y > -25.0f)
+		{
+			m_currentKeyFrame++;
+			m_timer = sf::Time::Zero;
+
+			
+		}
+		m_spritePosition.y = 300;
+	}
+}
+
+void AnimateSplash::slide(sf::Time t)
+{
+	sf::Vector2f moveText(m_slideTarget - m_textStart);
+
+	float scaler = m_slideDuration.asSeconds() / t.asSeconds();
+	moveText /= scaler;
+
+	m_textPosition += moveText;
+
+	m_timer += t;
+
+	if (m_timer >= m_slideDuration)
+	{
+		m_paused = true;
+		m_currentKeyFrame++;
+		m_textPosition = m_slideTarget;
+		m_timer = sf::Time::Zero;
+	}
+}
+
+void AnimateSplash::exit(sf::Time t)
+{
+	sf::Vector2f move(m_exitTarget - m_slideTarget);
+
+	float scaler = m_exitDuration.asSeconds() / t.asSeconds();
+	move /= scaler;
+
+	m_textPosition += move;
+	m_spritePosition += move;
+
+	m_timer += t;
+
+	if (m_timer >= m_exitDuration)
+	{
+		m_finished = true;
+		m_currentKeyFrame++;
+	}
 }
--- HCI-Typography/HCI-Typography/AnimateSplash.h
+++ HCI-Typography/HCI-Typography/AnimateSplash.h
@@ -15,6 +15,7 @@ public:
 	void render(sf::RenderWindow &window);
 	bool isPaused();
 	bool isFinished();
+	void play();
 
 private:
 	bool m_paused; //Is the animation paused
@@ -33,5 +34,15 @@ private:
 
 	int m_currentKeyFrame; //the current keyframe that is playing
 
-};
+	sf::Time m_slideDuration; //duration of slide keyframe
+	sf::Time m_exitDuration; //Durations of exit keyframe
+	sf::Time m_timer; //current time of keyframe
 
+	sf::Vector2f m_textStart; //Start position of text
+	sf::Vector2f m_slideTarget; //Targer position for slide keyframe
+	sf::Vector2f m_exitTarget; //Target position for exit keyframe
+
+	void fall(sf::Time t);
+	void slide(sf::Time t);
+	void exit(sf::Time t);
+};
